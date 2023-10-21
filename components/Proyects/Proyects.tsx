40bfeb7e0e7ee0c9a99578c1c1cbaae9cbd13dbd
RobotizeRPA <148357627+RobotizeRPA@users.noreply.github.com>
2023-10-21 19:27:16 -0300

    Update Proyects.tsx

--- components/Proyects/Proyects.tsx
+++ components/Proyects/Proyects.tsx
@@ -11,10 +11,10 @@ export default function Proyects(){
               <p className="text-left py-2 max-sm:text-center">RobotizeWeb es un diario virtual actualizado. Utiliza el sistema de "Web Scrapping" para obtener noticias de ultimo momento y las filtra a traves de Chat GPT para modificarlas y crear su propio formato.</p>
               <button className="text-center border-2 border-[#441eae] px-2 rounded-lg">Ver video</button>
             </div>
-            <Image src='https://res.cloudinary.com/vuelosflights/image/upload/v1697926909/arrendamiento_auitip.png' alt='RobotizeNews' width={450} height={150} className="border-2 border-[#441eae]"/>
+            <Image src='https://res.cloudinary.com/vuelosflights/image/upload/v1697926871/robotizenews_voibbf.png' alt='RobotizeNews' width={450} height={150} className="border-2 border-[#441eae]"/>
           </div>
           <div className="grid grid-cols-2 justify-items-center px-4 items-center gap-4 max-sm:grid-cols-1">
-            <Image src='../../assets/vuelos.png' alt='RobotizeFlight' width={450} height={150} className="border-2 border-[#441eae] max-sm:hidden"/>
+            <Image src='https://res.cloudinary.com/vuelosflights/image/upload/v1697926901/vuelos_wfaxcu.png' alt='RobotizeFlight' width={450} height={150} className="border-2 border-[#441eae] max-sm:hidden"/>
             <div>
               <h4 className="text-robotize font-semibold text-2xl py-2 text-right max-sm:text-center">Vuelos Robotize</h4>
               <p className="text-right py-2 max-sm:text-center">Vuelos Robotize se ejecuta cada treinta minutos, entra a la pagina donde se ven reflejados todos los vuelos de la empresa y chequea si hay nuevos vuelos agregados para asi notificarselo a los pilotos. De esta manera cada piloto puede estar al tanto y sacar el maximo provecho.</p>
